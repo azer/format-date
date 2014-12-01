3878c653de6d319af786230e0674dfd2a5f3c2d7
E.Azer Koçulu <azer@kodfabrik.com>
2014-11-30 21:03:56 -0800

    add utc

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var months = require("months");
 module.exports = formatDate;
 
 function formatDate (template, date) {
-  if (!date) return template;
+  if (!date) date = new Date;
 
   return format(template, {
     day: leftpad(date.getDate(), 2, '0'),
@@ -16,6 +16,16 @@ function formatDate (template, date) {
     minutes: leftpad(date.getMinutes(), 2, '0'),
     seconds: leftpad(date.getSeconds(), 2, '0'),
     'day-name': days[date.getDay()],
-    'month-name': months[date.getMonth()]
+    'month-name': months[date.getMonth()],
+
+    // utc
+    'utc-day': leftpad(date.getUTCDate(), 2, '0'),
+    'utc-month': leftpad(date.getUTCMonth() + 1, 2, '0'),
+    'utc-year': date.getUTCFullYear(),
+    'utc-hours': leftpad(date.getUTCHours(), 2, '0'),
+    'utc-minutes': leftpad(date.getUTCMinutes(), 2, '0'),
+    'utc-seconds': leftpad(date.getUTCSeconds(), 2, '0'),
+    'utc-day-name': days[date.getUTCDay()],
+    'utc-month-name': months[date.getUTCMonth()]
   });
 }
